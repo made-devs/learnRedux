e7dcc5ba6b6fb69675094fc3c99781c657e43535
madedev <nmaderama@gmail.com>
2024-09-04 11:59:49 +0700

    add more state: customer

--- learnRedux/src/store.jsx
+++ learnRedux/src/store.jsx
@@ -1,12 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: '',
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
@@ -20,7 +26,7 @@ function reducer(state = initialState, action) {
         loanPurpose: action.payload.purpose,
         balance: state.balance + action.payload.amount,
       };
-    case 'account/payLoanBack':
+    case 'account/payLoan':
       return {
         ...state,
         loan: 0,
@@ -32,18 +38,74 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case 'customer/updateCustomer':
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
-store.dispatch({ type: 'account/withdraw', payload: 200 });
-
-console.log(store.getState());
-
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: { amount: 1000, purpose: 'Pay a car' },
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
 });
-console.log(store.getState());
 
-store.dispatch({ type: 'account/payLoan' });
+const store = createStore(rootReducer);
+
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: { amount: 1000, purpose: 'Pay a car' },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: 'account/payLoan' });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, 'buy a car'));
+store.dispatch(payLoan());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: 'customer/updateName', payload: fullName };
+}
+
+store.dispatch(createCustomer('Ngakan Made Rama Devayana', '1235341312'));
 console.log(store.getState());
